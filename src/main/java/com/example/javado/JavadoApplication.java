3c21ccf3c21171bc0919dff58d976c1d6b07c3e0
ehgns <ehgns0125@naver.com>
2022-07-25 18:11:11 +0900

    dd

---
+++ src/main/java/com/example/javado/JavadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javado/JavadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
